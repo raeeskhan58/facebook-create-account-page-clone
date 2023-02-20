e812d15d57a21a8ef98fc454572f6115c0329883
raeesKhan <raeeskhanbuneri58@gmail.com>
2023-02-20 10:32:46 +0500

    facebook create account fage clone

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        13:"3.2rem",
+        98:"26rem",
+        100:"27rem",
+        105:"28.35rem",
+        110:"29.7rem",
+        115:"31.05rem",
+        120:"32.4rem",
+        125:"33.75rem",
+        130:"35.1rem",
+        135:"36.45rem",
+        140:"37.8rem",
+        145:"39.15rem",
+        150:"40.5rem",
+        155:"41.85rem",
+        160:"43.2rem",
+        165:"44.55rem",
+        170:"45.9rem",
+        175:"47.25rem",
+        180:"48.6rem"
+      }
+    },
+  },
+  plugins: [],
+}
